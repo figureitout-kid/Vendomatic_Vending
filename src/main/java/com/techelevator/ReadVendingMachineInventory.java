0a22020d295b675e43c29d1628e445c961039606
Braxton.Preslar <bnunjo11@gmail.com>
2023-10-06 10:35:10 -0600

    Exit functionality added & read from inventory file class begun

---
+++ src/main/java/com/techelevator/ReadVendingMachineInventory.java
@@ -0,0 +1,12 @@
+package com.techelevator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class ReadVendingMachineInventory {
+    public static void main(String[] args) {
+        String filePath = "module-1-capstone/vendingmachine.csv";
+    }
+}
--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,11 +2,15 @@ package com.techelevator;
 
 import com.techelevator.view.Menu;
 
+import java.io.File;
+
+
 public class VendingMachineCLI {
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	private static final String MAIN_MENU_OPTION_EXIT= "Exit";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT };
 
 	private Menu menu;
 
@@ -20,8 +24,18 @@ public class VendingMachineCLI {
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 				// display vending machine items
+				File itemsFile = new File("vendingmachine.csv");
+				if (itemsFile.exists()) {
+					//add code for if file exists
+				} else {
+					System.out.println("Vending machine inventory file not found.");
+					//add exception
+				}
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
+			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
+				System.out.println("Thank you for using this vending machine. Have a nice day...");
+				System.exit(0);
 			}
 		}
 	}
